16714769880c81f67ebf4171ff99c2963d18c78d
Daniel-FFF <danielhsu655@gmail.com>
2024-08-10 17:07:52 +0800

    Create styles.css

---
+++ styles.css
@@ -0,0 +1,43 @@
+h1{
+  display: flex;
+  
+  justify-content: center;    
+  
+  align-items: center;        
+}
+
+div {
+ 
+ 
+        width: 2486px;
+        height: 1300px;
+       
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #C6DDFA;
+}
+div2{
+  display: flex;
+
+  justify-content: center;    
+
+  align-items: center;    
+
+
+}
+
+
+div2{
+  position: relative;
+}
+h2{font-size: 50px;
+  color: rgb(0, 0, 0);
+  position:absolute;
+  top:25px;
+  left:200px;}
+ul{font-size: 20px;
+  position:absolute;
+  top:150px;
+  left:200px;
+}
+ 
